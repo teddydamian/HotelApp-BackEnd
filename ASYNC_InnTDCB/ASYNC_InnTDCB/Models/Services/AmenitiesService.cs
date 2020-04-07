c5fbbe3e8d7e9dab2b5ebe0c6b8318a849e9282a
teddydamian <teddy.damian@gmail.com>
2020-04-07 13:12:23 -0700

    All Done

---
+++ ASYNC_InnTDCB/ASYNC_InnTDCB/Models/Services/AmenitiesService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ASYNC_InnTDCB.Models.Interfaces;
+using ASYNC_InnTDCB.Properties.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASYNC_InnTDCB.Models.Services
+{
+ 
+        public class AmenitiesService : IAmenitiesManager
+    {
+            private ASYNCinnDbContext _context;
+
+            /// <summary>
+            /// Connecting DB to CRUD
+            /// </summary>
+            /// <param name="context"></param>
+            public AmenitiesService(ASYNCinnDbContext context)
+            {
+                _context = context;
+            }
+
+            /// <summary>
+            /// Create a new Amenities
+            /// </summary>
+            /// <param name="amenities"></param>
+            /// <returns></returns>
+            public async Task<Amenities> CreateAmenities(Amenities amenities)
+            {
+                _context.Amenities.Add(amenities);
+
+                await _context.SaveChangesAsync();
+                return amenities;
+            }
+
+            /// <summary>
+            /// Get all Amenities
+            /// </summary>
+            /// <returns></returns>
+            public async Task<List<Amenities>> GetAllAmenities()
+            {
+                return await _context.Amenities.ToListAsync();
+            }
+
+            /// <summary>
+            /// Get specific amenities by ID
+            /// </summary>
+            /// <param name="amenitiesID"></param>
+            /// <returns></returns>
+            public async Task<Amenities> GetAmenitieByID(int amenitiesID)
+            {
+            Amenities amenities = await _context.Amenities.FindAsync(amenitiesID);
+                return amenities;
+            }
+
+            /// <summary>
+            /// Delete amenities by ID
+            /// </summary>
+            /// <param name="amenitiesID"></param>
+            /// <returns></returns>
+            public async Task RemoveAmenities(int amenitiesID)
+            {
+            Amenities amenities = await GetAmenitieByID(amenitiesID);
+                _context.Amenities.Remove(amenities);
+                await _context.SaveChangesAsync();
+
+            }
+
+            /// <summary>
+            /// Update an amenity by ID
+            /// </summary>
+            /// <param name="amenitiesID"></param>
+            /// <param name="amenities"></param>
+            /// <returns></returns>
+            public async Task UpdateAmenities(int amenitiesID, Amenities amenities)
+            {
+                _context.Entry(amenities).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+        }
+    
+}
---
+++ ASYNC_InnTDCB/ASYNC_InnTDCB/Models/Services/RoomService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ASYNC_InnTDCB.Models.Interfaces;
+using ASYNC_InnTDCB.Properties.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASYNC_InnTDCB.Models.Services
+{
+    public class RoomService : IRoomManager
+    {
+        private ASYNCinnDbContext _context;
+
+        /// <summary>
+        /// Connecting DB to CRUD
+        /// </summary>
+        /// <param name="context"></param>
+        public RoomService(ASYNCinnDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Create a room
+        /// </summary>
+        /// <param name="room"></param>
+        /// <returns></returns>
+        public async Task<Room> CreateRoom(Room room)
+        {
+            _context.Rooms.Add(room);
+
+            await _context.SaveChangesAsync();
+            return room;
+        }
+
+        /// <summary>
+        /// Get all rooms
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Room>> GetAllRooms()
+        {
+            return await _context.Rooms.ToListAsync();
+        }
+
+        /// <summary>
+        /// Get a room by ID
+        /// </summary>
+        /// <param name="roomID"></param>
+        /// <returns></returns>
+        public async Task<Room> GetRoomByID(int roomID)
+        {
+            Room room = await _context.Rooms.FindAsync(roomID);
+            return room;
+        }
+
+        /// <summary>
+        /// Delete a Room
+        /// </summary>
+        /// <param name="roomID"></param>
+        /// <returns></returns>
+        public async Task RemoveRoom(int roomID)
+        {
+            Room room = await GetRoomByID(roomID);
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+
+        }
+
+        /// <summary>
+        /// Update the room
+        /// </summary>
+        /// <param name="roomID"></param>
+        /// <param name="room"></param>
+        /// <returns></returns>
+        public async Task UpdateRoom(int roomID, Room room)
+        {
+            _context.Entry(room).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Access to RoomAmenities from Hotel
+        /// </summary>
+        /// <param name="amenitiesId"></param>
+        /// <param name="roomId"></param>
+        /// <returns></returns>
+        public async Task<List<RoomAmenities>> GetAllRoomAmenities(int amenitiesId, int roomId)
+        {
+            return await _context.RoomAmenities.ToListAsync();
+        }
+    }
+}
